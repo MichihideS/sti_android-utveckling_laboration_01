a753ed7198483483e252e2d855b3df1e72abab8a
michihidesugito <Michihides@gmail.com>
2024-03-06 14:11:59 +0100

    added text based gacha

--- app/src/main/java/com/example/laboration_01/LoggedInActivity.kt
+++ app/src/main/java/com/example/laboration_01/LoggedInActivity.kt
@@ -1,7 +1,9 @@
 package com.example.laboration_01
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.laboration_01.databinding.ActivityAboutBinding
 import com.example.laboration_01.databinding.ActivityLoggedInBinding
 
 class LoggedInActivity : AppCompatActivity() {
@@ -14,9 +16,34 @@ class LoggedInActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val tvLoggedInWelcome = binding.tvLoggedInWelcome
+        val tvGachaResults = binding.tvGachaResults
+        val btnDraw = binding.btnDraw
 
         // Welcomes the user with the username used to log in
         tvLoggedInWelcome.text = intent.getStringExtra("welcome")
 
+        btnDraw.setOnClickListener {
+            tvGachaResults.text = gacha()
+            tvGachaResults.visibility = View.VISIBLE
+
+        }
+    }
+
+    private fun gacha(): String {
+        var threeStar = 0
+        var fourStar = 0
+        var fiveStar = 0
+
+        for (i in 1..10) {
+            val randomNumber = (1..1000).random()
+            when (randomNumber) {
+                in 1..6 -> fiveStar++
+                in 7..58 -> fourStar++
+                else -> threeStar++
+            }
+        }
+
+        return("Here are your draw results!\nNumber of 3* pulls: $threeStar\nNumber of 4* pulls:" +
+                " $fourStar\nNumber of 5* pulls: $fiveStar\nBetter luck next time!")
     }
 }
--- app/src/test/java/com/example/laboration_01/ExampleUnitTest.kt
+++ app/src/test/java/com/example/laboration_01/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.laboration_01
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.Random
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,20 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun gacha(){
+        var threeStar = 0
+        var fourStar = 0
+        var fiveStar = 0
+
+        for (i in 1..10) {
+            val randomNumber = (1..1000).random()
+            when (randomNumber) {
+                in 1..6 -> fiveStar++
+                in 7..58 -> fourStar++
+                else -> threeStar++
+            }
+        }
+
+        println("You got $threeStar 3* rolls, $fourStar 4* rolls and $fiveStar 5* rolls")
     }
 }
